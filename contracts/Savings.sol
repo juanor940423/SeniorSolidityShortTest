adf3344d29388f44d53c5f6226376f5b4a44e6e3
Juan Manuel <consultoriappmovil@gmail.com>
2022-03-14 18:13:53 -0500

    correcion en manejo de datos y mejora en funcion de calculo de interes

--- contracts/Savings.sol
+++ contracts/Savings.sol
@@ -1,16 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
 import "./IBank.sol";
 
 contract Savings is IBank{
 
-    mapping (address => Account) Account_user;
+    
+    uint256 public percent = 3; //3%
+    uint256 count_blocks = 100; //Total de bloques para interes del 3%
+
+    mapping (address => Account) Account_user; //Asingnacion de usuario por wallet
 
     function deposit(uint256 amount) external payable override returns (bool){
         
         require(amount > 0, "Debes depositar un monto mayor a 0");
-        require(msg.value > 0 ether, "Debes depositar un monto mayor a 0");        
-        Account_user[msg.sender]=Account(amount,block.number); //Asignacion a una wallet del monto depositado y el numero del bloque actual
+        require(msg.value > 0 ether, "Debes depositar un monto mayor a 0");   
+        Account_user[msg.sender]=Account(amount * 1 ether,block.number); //Asignacion a una wallet del monto depositado y el numero del bloque actual
 
 
         emit Deposit(msg.sender,amount);
@@ -29,12 +34,18 @@ contract Savings is IBank{
     }
     function getBalance() external view override returns (uint256){        
         uint256 balanceUser = Account_user[msg.sender].deposit;
+        balanceUser += InterstAccrued(); // Actualizacion del deposito + intereses
         return balanceUser;
     }
 
-     function InterstAccrued() private view {
-        uint256 blocks =block.number - Account_user[msg.sender].lastInterestBlock;
-        uint256 rate = blocks/100;
+     function InterstAccrued() private view returns(uint256){
+        uint256 blocks =(block.number - Account_user[msg.sender].lastInterestBlock); //Calculo del total de bloques transcurridos
+        uint256 rate = blocks*(percent*10**16)/count_blocks; //Calculo de la tasa de interes hasta el bloque actual
+        uint256 interstTotal = (Account_user[msg.sender].deposit / 1 ether)*rate; //Calculo del total de intereses obtenidos hasta el bloque actual
+
+        return interstTotal;
+
+        
     }
 
 
